8e2a632eac011dc7ba498dc251fdbc642d8d93c6
idbolshakov <idbolshakov@gmail.com>
2018-03-23 02:42:12 +0500

    initial commit

---
+++ src/api/products.js
@@ -0,0 +1,73 @@
+var API = {
+  products: [
+    {
+      "id": 1, 
+      "title": "Выкатной детский диван Зайка производитель фабрика Blanes", 
+      "price": 11740,
+      "img": "assets/images/1.jpg"
+    },
+
+    {
+      "id": 2, 
+      "title": "Диван Банжо", 
+      "price": 62839,
+      "img": "assets/images/2.jpg"
+    },
+    
+    {
+      "id": 3, 
+      "title": "Гостиная классика Panamar Classic", 
+      "price": 267593,
+      "img": "assets/images/3.jpg"
+    },
+
+    {
+      "id": 4, 
+      "title": "Chairman Диван Релакс Terra 101", 
+      "price": 31950,
+      "img": "assets/images/4.jpg"
+    },
+
+    {
+      "id": 5, 
+      "title": "Диван матрас прямой Верн Sleepformer", 
+      "price": 52450,
+      "img": "assets/images/5.jpg"
+    },
+
+    {
+      "id": 6, 
+      "title": "2 кресла и стол чайный - комплект «Виктория» (Эдем)", 
+      "price": 62350,
+      "img": "assets/images/6.jpg"
+    },
+    
+    {
+      "id": 7, 
+      "title": "Современный стильный угловой диван Flex с декоративной столешницей", 
+      "price": 483000,
+      "img": "assets/images/7.jpg"
+    },
+
+    {
+      "id": 8, 
+      "title": "Белый диван Deco - Colleccion Alexandra", 
+      "price": 606400,
+      "img": "assets/images/8.jpg"
+    }, 
+    
+    {
+      "id": 9, 
+      "title": "Белый диван в гостиную с цветной обивкой", 
+      "price": 394899,
+      "img": "assets/images/9.jpg"
+    },
+
+    {
+      "id": 10, 
+      "title": "Двухместный бархатный диван", 
+      "price": 13240,
+      "img": "assets/images/10.jpg"
+    },           
+  ]
+};
